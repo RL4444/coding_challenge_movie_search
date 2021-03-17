b9768852d5d713a83fbf7fb099f71121ba43546d
Rory Lewis <lewisroryjames@gmail.com>
2021-03-17 13:01:56 +0100

    Basic CRA setup

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState, FC, useEffect, ChangeEvent } from 'react';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import { Layout, Title } from './styled/index';
+
+import MovieList from './components/MovieList';
+import Searchbar from './components/Searchbar';
+import Spinner from './components/Spinner';
+
+const App: FC = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [isTyping, setIsTyping] = useState(false);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value);
+
+        // creates a boolean that will restrict api from being called too often
+        if (isTyping) return;
+        setIsTyping(true);
+        setTimeout(() => {
+            setIsTyping(false);
+        }, 2000);
+    };
+
+    const clearSearchBar = () => setSearchTerm('');
+
+    return (
+        <Layout>
+            <Title>MOVIEBOOK</Title>
+            <Searchbar searchTerm={searchTerm} clear={clearSearchBar} change={handleChange} />
+            {searchTerm.length > 0 && !isTyping && <MovieList searchTerm={searchTerm} />}
+            {isTyping && searchTerm.length > 0 && (
+                <div className='mt-2'>
+                    <Spinner />
+                </div>
+            )}
+        </Layout>
+    );
+};
+
+export default App;
