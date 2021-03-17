3b17be0034e27d3ee174f2bb249ecf694010445a
Rory Lewis <lewisroryjames@gmail.com>
2021-03-17 13:02:56 +0100

    types for movie api response added

---
+++ src/types/movie.ts
@@ -0,0 +1,20 @@
+export interface IMovieApi {
+    Poster: string;
+    Title: string;
+    Type: string;
+    Year: string;
+    imdbID: string;
+}
+
+interface IApiSuccessResponse {
+    Error?: string
+    Response: string;
+    Search?: Array <IMovieApi>;
+    totalResults?: string;
+}
+
+export interface IApiResponse {
+    error?: string;
+    loading: boolean;
+    response: IApiSuccessResponse;
+}
