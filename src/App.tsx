b16d0af14754763dd8ad816678d1da17b272e0c9
Rory Lewis <lewisroryjames@gmail.com>
2021-03-17 13:14:53 +0100

    removed React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, FC, useEffect, ChangeEvent } from 'react';
 import styled from 'styled-components';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import { Layout, Title } from './styled/index';
 
